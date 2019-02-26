fa76afee406082e17a50e3705254d932fa46d4ed
Chanan Sukjirawanit <fort2002@hotmail.com>
2019-02-26 14:38:25 +0700

    Update

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ if (!is_null($events['events'])) {
                             $fouth_array = array();
                             $fifth_array = array();
                             $sixth_array = array();
-                            array_push($first_array, $name);
+                            array_push($first_array, $Name);
 
                             $messages = [
                                 [
